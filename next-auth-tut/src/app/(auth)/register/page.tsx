b1dcb4fecdcf0f966b08eb9b7c252016d9d931da
NazaboyZ <josefleite.00@hotmail.com>
2024-11-24 14:14:43 +0100

    login and register

--- next-auth-tut/src/app/(auth)/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RegisterForm } from "@/components/auth/RegisterForm";
-
-export default function LoginPage() {
-  return <RegisterForm />;
-}
--- next-auth-tut/src/services/email/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./sendEmailVerification";
-export * from "./sendPasswordReset";
